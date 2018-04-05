39fd443309b16819bcbe320bc45ab37b86f600f7
Szymon Oleszek <szymqwerty@gmail.com>
2018-04-05 15:19:45 +0200

    first task in JS

---
+++ scripts.js
@@ -0,0 +1,11 @@
+alert("uwazaj")
+
+
+var firstVariable = 15
+var secondVariable = "szymon"
+
+
+
+console.log(typeof secondVariable)
+console.log(typeof firstVariable)
+document.write(typeof firstVariable )
