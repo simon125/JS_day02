7a8ef290884a5c8778677da792efd307784ad7ca
Szymon Oleszek <szymqwerty@gmail.com>
2018-04-06 11:09:11 +0200

    3 tasks done

--- scripts.js
+++ scripts.js
@@ -7,3 +7,21 @@ var result = age2 && 1
 
 if(result) document.getElementById("score").innerHTML = "zawsze sie spelni ten warunek bo ma jedynke"
 else document.getElementById("score").innerHTML = "tak dla zasady zmieniam na &&"
+
+
+
+///////////////////////////////////////////task 2
+
+
+var variable = 4
+
+if(variable === 1) document.getElementById("score1").innerHTML = "return 1"
+else if(variable === '1') document.getElementById("score1").innerHTML = "return 2"
+else if(variable == 2) document.getElementById("score1").innerHTML = "return 3"
+else document.getElementById("score1").innerHTML = "WTF!!?!?!?!??!"
+
+
+////////////////////////////////////////////task 3
+
+
+
