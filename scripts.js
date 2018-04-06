ba1f6a5bd7432bfc6f35904328413b1587f72208
Szymon Oleszek <szymqwerty@gmail.com>
2018-04-06 10:50:48 +0200

    completed teplate for JS tasks

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
-alert("uwazaj")
+var age1 = false
+var age2 = null
 
 
-var firstVariable = 15
-var secondVariable = "szymon"
+var result = age2 && 1
 
 
-
-console.log(typeof secondVariable)
-console.log(typeof firstVariable)
-document.write(typeof firstVariable )
+if(result) document.getElementById("score").innerHTML = "zawsze sie spelni ten warunek bo ma jedynke"
+else document.getElementById("score").innerHTML = "tak dla zasady zmieniam na &&"
